702b044591d3d9697842ce6ed5f30578528ebb3e
Justice Isreal <justiceyba@gmail.com>
2023-06-01 00:10:12 +0100

    homepage client session completed

---
+++ components/Asset/Clientlogos.jsx
@@ -0,0 +1,8 @@
+const Clientlogos = [
+  { id: 1, logo: "/client1.png" },
+  { id: 2, logo: "/client2.png" },
+  { id: 3, logo: "/client3.png" },
+  { id: 4, logo: "/client4.png" },
+];
+
+export default Clientlogos;
--- components/Homepage/Clients.jsx
+++ components/Homepage/Clients.jsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
 import React from "react";
-
+import Clientlogos from "../Asset/Clientlogos";
 function Clients() {
-  return <div>client</div>;
+  return (
+    <div className="client-main-con">
+      <h1>
+        Over <span>100+ clients</span> nation wide
+      </h1>
+      <div className="client-logo">
+        {Clientlogos.map((item) => (
+          <Image
+            src={item.logo}
+            key={item.id}
+            alt="img"
+            className="img"
+            width={150}
+            height={115}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Clients;
