f398868820a0121dfc43d21a8b4282b8d59e1581
Justice Isreal <justiceyba@gmail.com>
2023-06-01 12:49:58 +0100

    homepage benefit session completed

--- components/Homepage/Benefits.jsx
+++ components/Homepage/Benefits.jsx
@@ -7,28 +7,9 @@ import { MdOutlineEmail } from "react-icons/md";
 
 function Benefits() {
   return (
-    <div>
-      <div>
-        <h3>
-          We are avaliable 24 / 7 to attend to you, contact us and get free
-          estimate
-        </h3>
-
-        <div className="contact">
-          <div className="contact-ions">
-            <div className="click">
-              <p>Click</p>
-              <ImPointRight className="icon" />
-            </div>
-            <FiPhone className="icons" />
-            <MdOutlineEmail className="icons" />
-            <FaWhatsapp className="icons" />
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div>
+    <div className="benefit-main-con">
+      <div className="benefit-con">
+        <div className="benefit-first-con">
           <h1>Why Choose Us For Your Services</h1>
           <p>
             At Calm Waters, our aim is to use the special skills acquired by
@@ -36,10 +17,51 @@ function Benefits() {
             income while developing long-term technological and managerial
             capabilities for our people and host communities.
           </p>
-          <Link href="/">Lets Talk</Link>
+          <Link href="/" className="btn">
+            <div>Lets Talk</div>{" "}
+          </Link>
         </div>
-        <div>
-          <div></div>
+        <div className="benefit-second-con">
+          <div className="benefit benefit1 benefit2">
+            <img src="/customerservice.png" alt="" />
+            <div className="detail">
+              <h3>24/7 Service</h3>
+              <p>
+                We are avaliable 24/7 with great customer service relations to
+                attend to all your enquiries
+              </p>
+            </div>
+          </div>
+          <div className="benefit benefit1 ">
+            <img src="/expert.png" alt="" />
+            <div className="detail">
+              <h3>Proffessionals</h3>
+              <p>
+                We are comprise of the best team of proffessionals to give hight
+                quality services
+              </p>
+            </div>
+          </div>
+          <div className="benefit benefit2">
+            <img src="/bestsolution.png" alt="" />
+            <div className="detail">
+              <h3>Tech Solution</h3>
+              <p>
+                We are apply technologically driven solutions ,to ensure quality
+                ,acuracy and state of the art solutions
+              </p>
+            </div>
+          </div>
+          <div className="benefit">
+            <img src="/provenrecord.png" alt="" />
+            <div className="detail">
+              <h3>Track record</h3>
+              <p>
+                Over the years we have accomplished great feets with excellent
+                proven track records to show
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
