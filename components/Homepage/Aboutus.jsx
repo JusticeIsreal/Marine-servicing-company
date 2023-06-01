0b61e3b92fbb925e2014b6a4419fb8f53c7ae0f0
Justice Isreal <justiceyba@gmail.com>
2023-06-01 02:07:23 +0100

    homepage about session completed

--- components/Homepage/Aboutus.jsx
+++ components/Homepage/Aboutus.jsx
@@ -1,7 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Aboutus() {
-  return <div>About</div>;
+  return (
+    <div className=" homeabout-main-con">
+      <h1>
+        <span>About Us</span>
+      </h1>
+
+      <div className="client-logo">
+        <div className="img">
+          <h3>100+</h3>
+          <p>Esteem clients</p>
+        </div>
+        <div className="img">
+          <h3>1k+</h3>
+          <p>Customer Review</p>
+        </div>
+        <div className="img">
+          <h3>10+</h3>
+          <p>Years Experience</p>
+        </div>
+        <div className="img">
+          <h3>100K+</h3>
+          <p>Completed Projects</p>
+        </div>
+      </div>
+      <div className="lower-part">
+        <Image
+          src="/abt.jpg"
+          alt="img"
+          width={350}
+          height={300}
+          className="lower-img"
+        />
+
+        <div className="lower-txt">
+          <p>
+            Calm Waters Integrated Services Limited is a Nigerian oil & gas,
+            Maritime support services and Logistics Company. The need for
+            security, risk assessment, quality assurance and efficient energy
+            supply across the world cannot be overemphasized; we at Calm Waters
+            are poised to provide excellent services tailored to meet our
+            clients’ needs. We Leverage our wealth of experience in the oil and
+            gas industry, shipping and ship management, ship security, port
+            facility security, procurement and maintenance services, to provide
+            seamless services to our clients, in line with industry best
+            practices and sustainability principles.
+          </p>
+          <button>Know more</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Aboutus;
