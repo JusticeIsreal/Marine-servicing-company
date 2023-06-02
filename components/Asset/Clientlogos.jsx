8f633f51c3f21c0331f291f53ed0db6ad17a63d5
Justice Isreal <justiceyba@gmail.com>
2023-06-02 13:36:24 +0100

    completed

--- components/Asset/Clientlogos.jsx
+++ components/Asset/Clientlogos.jsx
@@ -1,8 +1,20 @@
 const Clientlogos = [
-  { id: 1, logo: "/client4.png" },
-  { id: 2, logo: "/client5.png" },
-  { id: 3, logo: "/client6.png" },
-  { id: 4, logo: "/client7.png" },
+  {
+    id: 1,
+    logo: "https://res.cloudinary.com/isreal/image/upload/v1685708604/calm%20waters%20project/rahman-removebg-preview_evot8o.png",
+  },
+  {
+    id: 2,
+    logo: "https://res.cloudinary.com/isreal/image/upload/v1685708604/calm%20waters%20project/npa-stretch-banner-removebg-preview_yp4ypr.png",
+  },
+  {
+    id: 3,
+    logo: "https://res.cloudinary.com/isreal/image/upload/v1685708603/calm%20waters%20project/NIMASSA-LOGO-removebg-preview_e8j1ij.png",
+  },
+  {
+    id: 4,
+    logo: "https://res.cloudinary.com/isreal/image/upload/v1685708603/calm%20waters%20project/Auspoint_Final_NEW-Logo_Final_Website_23yrs-removebg-preview_cvfhc2.png",
+  },
 ];
 
 export default Clientlogos;
