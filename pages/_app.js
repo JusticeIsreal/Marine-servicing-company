a78a450eb3a46664d5d3ebaa4e9eb5d3d1e3dc18
Justice Isreal <justiceyba@gmail.com>
2023-07-02 01:02:30 +0100

    analysyis

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import "../styles/Homepage/style.css";
 import "../styles/Aboutpage/style.css";
 import "../styles/Servicepage/style.css";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} /> <Analytics />{" "}
+    </>
+  );
 }
